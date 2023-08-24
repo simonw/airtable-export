f1f9169604b5368450f243432ec1268c609eb6b9
Pedro Lopez Mareque <pedro.lopez.mareque@gmail.com>
2023-08-24 17:58:51 +0200

    feat: refactor code

--- airtable_export/exporter/csv_exporter.py
+++ airtable_export/exporter/csv_exporter.py
@@ -11,6 +11,6 @@ class CsvExporter:
             header = records[0].keys()
             writer.writerow(header)
             for record in records:
-                data = [value for value in record.values()]
-                writer.writerow(data)
+                row = [value for value in record.values()]
+                writer.writerow(row)
         return csv_filepath.name
